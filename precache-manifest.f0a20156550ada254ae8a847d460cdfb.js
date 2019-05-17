7fa082cdc47c74bfca36a33406189052a9dcab80
Ela Synowiec <esynowiec@gmail.com>
2019-05-17 11:48:52 +0200

    Updates

---
+++ precache-manifest.f0a20156550ada254ae8a847d460cdfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4a34caba1acc91ef093dee746d79bc2",
+    "url": "/Krakow-air-condition/static/media/LogoBlue.a4a34cab.svg"
+  },
+  {
+    "revision": "ae7f114b9d31a4823d15",
+    "url": "/Krakow-air-condition/static/js/runtime~main.ae7f114b.js"
+  },
+  {
+    "revision": "da8b9db6ddd2a235734c",
+    "url": "/Krakow-air-condition/static/js/main.da8b9db6.chunk.js"
+  },
+  {
+    "revision": "d25657e93ba5e9c40222",
+    "url": "/Krakow-air-condition/static/js/1.d25657e9.chunk.js"
+  },
+  {
+    "revision": "da8b9db6ddd2a235734c",
+    "url": "/Krakow-air-condition/static/css/main.21e1cafd.chunk.css"
+  },
+  {
+    "revision": "d25657e93ba5e9c40222",
+    "url": "/Krakow-air-condition/static/css/1.2f700c60.chunk.css"
+  },
+  {
+    "revision": "833748d533f4bd2fb9506640e6898ea1",
+    "url": "/Krakow-air-condition/index.html"
+  }
+];
